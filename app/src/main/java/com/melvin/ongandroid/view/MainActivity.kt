9043f0222299e9c6d36c86cbdfef07207be45273
LucasBaccaro1 <lucasbaccaro@hotmail.com>
2022-01-26 16:45:17 -0300

    layout-login-designe

--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -7,6 +7,6 @@ import com.melvin.ongandroid.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_layout)
     }
 }
