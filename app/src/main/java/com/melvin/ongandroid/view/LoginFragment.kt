6d105f2c8d201ecd564f73e1adea816dded576b6
Néstor del Río <nestordelrio1989@gmail.com>
2022-02-07 18:25:18 -0300

    Toast coming soon

--- app/src/main/java/com/melvin/ongandroid/view/LoginFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/LoginFragment.kt
@@ -7,12 +7,14 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.businesslogic.data.DataSource
+import com.melvin.ongandroid.businesslogic.vo.MainApplication
 import com.melvin.ongandroid.databinding.FragmentLoginBinding
 import com.melvin.ongandroid.model.repository.RepoImpl
 import com.melvin.ongandroid.viewmodel.UserViewModel
@@ -119,7 +121,7 @@ class LoginFragment : Fragment() {
             if (it != null) {
                 if (it.success) {
                     _binding!!.prBar.visibility=View.GONE
-                    // ir al HOME
+                    Toast.makeText(MainApplication.applicationContext(),"Proximamente sección intro", Toast.LENGTH_LONG).show()
                 } else {
                     _binding!!.prBar.visibility=View.GONE
                     _binding!!.tvEmail.error = getString(R.string.login_et_error_user_and_password)
