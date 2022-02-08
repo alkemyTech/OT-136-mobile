d73b6bd313b6f088757fe7451e7d083fb48e05fa
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2022-02-08 15:34:23 -0300

    delete unnecessary changes

--- app/src/main/java/com/melvin/ongandroid/businesslogic/data/DataSource.kt
+++ app/src/main/java/com/melvin/ongandroid/businesslogic/data/DataSource.kt
@@ -1,10 +1,10 @@
 package com.melvin.ongandroid.businesslogic.data
-
 import android.app.AlertDialog
 import android.content.Context
 import android.widget.Toast
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.businesslogic.domain.OnRequest
+import com.melvin.ongandroid.businesslogic.vo.MainApplication
 import com.melvin.ongandroid.businesslogic.vo.RetrofitClient
 import com.melvin.ongandroid.model.DefaultResponse
 import com.melvin.ongandroid.model.User
@@ -16,19 +16,14 @@ import retrofit2.Response
 import retrofit2.HttpException
 import java.io.IOException
 import java.net.UnknownHostException
-
 class DataSource {
-
-
     suspend fun postRegister(user: User, context: Context?, onResponse: OnAPIResponse){
-
         RetrofitClient.retrofitService.createUser(user)
             .enqueue(object: Callback<DefaultResponse>, OnRequest{
                 override fun onResponse(
                     call: Call<DefaultResponse>,
                     response: Response<DefaultResponse>
                 ) {
-
                     Toast.makeText(context,"User was succesfully register",
                         Toast.LENGTH_LONG).show()
                     if(response.isSuccessful){
@@ -36,12 +31,10 @@ class DataSource {
                     } else {
                         onResponse.onLoading(response)
                     }
-
                 }
                 override fun onFailure(call: Call<DefaultResponse>, t: Throwable) {
                     onResponse.onFailure("The user cannot be registered")
                     Toast.makeText(context,"The user cannot be registered", Toast.LENGTH_LONG).show()
-
                     if (t is HttpException){
                         when (t.code()) {
                             400 -> dialogBuilder(context,R.string.dataSource_dialogBuilder_title_error,R.string.login_dg_bad_request)
@@ -56,21 +49,17 @@ class DataSource {
                     }
                 }
 
-                override fun dialogBuilder(context: Context?, title:Int, message:Int) {
-
-                    val builder = AlertDialog.Builder(context!!)
+                override fun dialogBuilder(context: Context?,title:Int,message:Int) {
+                    val builder = AlertDialog.Builder(context)
                     builder.setTitle(title)
                     builder.setMessage(message)
                     builder.setPositiveButton(R.string.ok) {
                             dialog, which ->
                     }
-
                     builder.show()
                 }
-
             })
     }
-
     suspend fun authUser(user: String, pass: String): Response<VerifyUser> {
         return RetrofitClient.retrofitService.postLogin(user, pass)
     }
