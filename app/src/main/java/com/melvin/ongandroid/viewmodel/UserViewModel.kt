61b82f09540f174c7987747bc9165f632caa9939
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2022-02-08 15:21:51 -0300

    delete unnecessary functions from UserViewModel

--- app/src/main/java/com/melvin/ongandroid/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/UserViewModel.kt
@@ -1,18 +1,14 @@
 package com.melvin.ongandroid.viewmodel
-
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.melvin.ongandroid.model.repository.Repo
 import com.melvin.ongandroid.model.response.VerifyUser
 import kotlinx.coroutines.launch
-
 class UserViewModel(private val repo: Repo) : ViewModel() {
 
     val liveDataUser = MutableLiveData<VerifyUser>()
     val authException = MutableLiveData<Throwable>()
-    val validEmail = MutableLiveData<Boolean>()
-    val validPassword = MutableLiveData<Boolean>()
 
     fun postToken(user: String, pass: String) {
         viewModelScope.launch {
@@ -21,31 +17,9 @@ class UserViewModel(private val repo: Repo) : ViewModel() {
                 if (call.isSuccessful) {
                     liveDataUser.postValue(call.body())
                 }
-            } catch(e:Exception) {
+            } catch (e: Exception) {
                 authException.value = e
             }
         }
-    }
-    fun validateEmailAndPassword(email: String?,password:String?):Boolean {
-        validEmail.value = validateEmail(email)
-        validPassword.value = validatePassword(password)
-        return validEmail.value!! && validPassword.value!!
-    }
-
-
-    fun validateEmail(email: String?):Boolean {
-        validEmail.value = android.util.Patterns.EMAIL_ADDRESS.matcher(email.toString()).matches()
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email.toString()).matches()
-    }
-
-    fun validatePassword(password:String?):Boolean {
-        return if (!password.isNullOrEmpty()) {
-            validPassword.value = password.length >= 4
-            password.length >= 4
-        } else {
-            validPassword.value = false
-            false
-        }
-
     }
 }
