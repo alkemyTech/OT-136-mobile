be6c71222adf42004947e9210fcce5ff8b25fe9d
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2022-02-11 19:20:33 -0300

    Add arrow to end of slider

--- app/src/main/java/com/melvin/ongandroid/view/TestimonialsAdapter.kt
+++ app/src/main/java/com/melvin/ongandroid/view/TestimonialsAdapter.kt
@@ -2,18 +2,25 @@ package com.melvin.ongandroid.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.ItemTestimonialHomeBinding
 import com.melvin.ongandroid.model.response.TestimonialData
 import com.melvin.ongandroid.model.response.Testimonials
 import com.squareup.picasso.Picasso
 
 class TestimonialsAdapter(private var testimonials: Testimonials)  : RecyclerView.Adapter<TestimonialsAdapter.ViewHolder>(){
-    class ViewHolder (private val binding: ItemTestimonialHomeBinding) : RecyclerView.ViewHolder(binding.root) {
 
+    class ViewHolder (private val binding: ItemTestimonialHomeBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(testimonial: TestimonialData){
             binding.tvDesc.text = testimonial.description
             Picasso.get().load(testimonial.image).into(binding.ivTestimonial)
+        }
+        fun showArrow(){
+            binding.cvItem.setContentPadding(40,40,40,40)
+            binding.tvDesc.isVisible = false
+            binding.ivTestimonial.setImageResource(R.drawable.ic_baseline_arrow_right_24)
         }
     }
 
@@ -26,11 +33,13 @@ class TestimonialsAdapter(private var testimonials: Testimonials)  : RecyclerVie
     }
 
     override fun onBindViewHolder(holder:ViewHolder, position: Int) {
-        holder.bind(testimonials.data[position])
+        if (position <= testimonials.data.lastIndex){
+            holder.bind(testimonials.data[position])
+        }else { holder.showArrow() }
     }
 
     override fun getItemCount(): Int {
-        return testimonials.data.size
+        return testimonials.data.size + 1
     }
 
 }
