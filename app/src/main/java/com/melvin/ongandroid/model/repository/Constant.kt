d8449936f51365e26aa511f6b967712c8dabc925
Néstor del Río <nestordelrio1989@gmail.com>
2022-02-17 20:36:05 -0300

    added Google signin to login

--- app/src/main/java/com/melvin/ongandroid/model/repository/Constant.kt
+++ app/src/main/java/com/melvin/ongandroid/model/repository/Constant.kt
@@ -5,5 +5,6 @@ class Constant {
         val PREF_IS_LOGIN = "PREF_IS_LOGIN"
         val PREF_USERNAME = "PREF_USERNAME"
         val PREF_PASSWORD = "PREF_PASSWORD"
+        const val RC_SIGN_IN = 136
     }
 }
