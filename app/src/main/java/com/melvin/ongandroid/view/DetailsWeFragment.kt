33d81b57b01a7aaa1a5e76681f6cf7ff53fb5327
Néstor del Río <nestordelrio1989@gmail.com>
2022-02-17 14:32:28 -0300

    intents to social media added

--- app/src/main/java/com/melvin/ongandroid/view/DetailsWeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/DetailsWeFragment.kt
@@ -30,7 +30,6 @@ class DetailsWeFragment: Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentDetailsWeBinding.inflate(inflater, container, false)
         return binding.root
     }
