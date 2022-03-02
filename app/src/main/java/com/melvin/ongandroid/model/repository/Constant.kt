71c00bb10383265da806304bcfafe2ab46afe1a9
Brenda Rodriguez <rodriguez.brenm@gmail.com>
2022-03-02 01:16:33 -0300

    Agrega variable NAME a sharedPreference para guardar el nombre al iniciar sesion con usuario y contraseña. Este luego lo utiliza en Home.

--- app/src/main/java/com/melvin/ongandroid/model/repository/Constant.kt
+++ app/src/main/java/com/melvin/ongandroid/model/repository/Constant.kt
@@ -2,9 +2,10 @@ package com.melvin.ongandroid.model.repository
 
 class Constant {
     companion object {
-        val PREF_IS_LOGIN = "PREF_IS_LOGIN"
-        val PREF_USERNAME = "PREF_USERNAME"
-        val PREF_PASSWORD = "PREF_PASSWORD"
+        const val PREF_IS_LOGIN = "PREF_IS_LOGIN"
+        const val PREF_USERNAME = "PREF_USERNAME"
+        const val PREF_PASSWORD = "PREF_PASSWORD"
+        const val PREF_NAME = "PREF_NAME"
         const val RC_SIGN_IN = 9001
     }
 }
